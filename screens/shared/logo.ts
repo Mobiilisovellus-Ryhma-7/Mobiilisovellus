7e432176e3fb519d36f33673fb97d350b95d010d
Valtteri Rehu <90039969+Valde00@users.noreply.github.com>
2026-04-08 14:26:34 +0300

    logokikkeli

--- screens/shared/logo.ts
+++ screens/shared/logo.ts
@@ -1,5 +1,5 @@
-export const dynamicSportHallLogoLight = require('../../assets/dynamic-sport-hall-logo.png');
-export const dynamicSportHallLogoDark = require('../../assets/dynamic-sport-hall-logo.png');
+export const dynamicSportHallLogoLight = require('../../assets/Dynamic_Sport_Hall_Logo_in_Blue_and_Orange kopio.png');
+export const dynamicSportHallLogoDark = require('../../assets/Dynamic_Sport_Hall_Logo_in_Blue_and_Orange Dark.png');
 
 export function getDynamicSportHallLogoSource(isDarkMode: boolean) {
 	return isDarkMode ? dynamicSportHallLogoDark : dynamicSportHallLogoLight;
